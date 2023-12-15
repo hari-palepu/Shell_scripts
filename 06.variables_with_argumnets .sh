8f0a12787ce4e35845028a520aedd6eec066beeb
hari <hari@gmail.com>
2023-12-15 11:34:59 +0000

    chnges

--- 06.variables_with_argumnets .sh
+++ 06.variables_with_argumnets .sh
@@ -6,7 +6,7 @@
 NAME01=$1
 NAME02=$2
 
-echo "Conversation between Rohit & Ravi"
+echo "Conversation between $NAME01 & $NAME02"
 echo "$NAME01: Hi $NAME02, How are you?"
 echo "$NAME02: Hello $NAME01, I am doing good! How are you?"
 echo "$NAME01: I am fine $NAME02"
