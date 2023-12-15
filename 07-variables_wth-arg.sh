6cd3cae9cdd61c71ff78c0b34632e766430f381b
hari <hari@gmail.com>
2023-12-15 14:57:11 +0000

    msg

--- 07-variables_wth-arg.sh
+++ 07-variables_wth-arg.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#We use the arguments to  give passwords during run time instead of saving it in the file.
+#We can use the arguments to  give passwords during run time instead of saving it in the file.
 #How to enter commands during run time use "read" command. 
 
 echo "Please enter your name"
-#read command 
- read DATA # The name entered above will be automatically attached to USERNAME variable.
+#The Linux read command is used to read the contents of a line into a variable
+read USERNAME  # The name entered above will be automatically attached to USERNAME variable.
+echo "The user name is: $USERNAME"
 
- echo "$DATA"
+echo "Please enter your password"
+read PASSWORD  
+echo "The password is: $PASSWORD"
