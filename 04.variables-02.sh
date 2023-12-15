0bf72665a36722a34121a8bef207f6ad70c2c74e
hari <hari@gmail.com>
2023-12-15 10:12:48 +0000

    Testing variable

---
+++ 04.variables-02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAME01=Hari
+NAME02={giri}
+
+echo "$NAME01: Hi $NAME02, How are you?"
+echo "$NAME02: Hello $NAME01, I am doing good! How are you?"
+echo "$NAME01: I am fine $NAME02"
