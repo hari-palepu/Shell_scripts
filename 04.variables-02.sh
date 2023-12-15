2b840f162790ad6c9a1568095c92fefda8a1bc3a
hari <hari@gmail.com>
2023-12-15 10:44:37 +0000

    chnges

--- 04.variables-02.sh
+++ 04.variables-02.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+#Variable syntax is VAR=DATA but here VAR=NAME and DATA=Hari
+#Var names are not mandatory to give capital we can give lower case as well.......
 NAME01=Hari
-NAME02=giri
+NAME02=Ravi
 
+#Two ways to access Variables, $varname i.e, $NAME01 or ${NAME01}.......
 echo "$NAME01: Hi $NAME02, How are you?"
 echo "$NAME02: Hello $NAME01, I am doing good! How are you?"
 echo "$NAME01: I am fine $NAME02"
 
 NAME01=Ramesh
 NAME02=Suresh
-${NAME02}
-echo "$NAME01: Hi ${NAME02}, How are you?"
-echo "${NAME02}: Hello $NAME01, I am doing good! How are you?"
-echo "$NAME01: I am fine ${NAME02}"
+
+echo "${NAME01}: Hi ${NAME02}, How are you?"
+echo "${NAME02}: Hello ${NAME01}, I am doing good! How are you?"
+echo "${NAME01}: I am fine... ${NAME02}"
+echo "${NAME02}: When are comming to met me"
+echo "${NAME01}: yeah ${NAME02}...I will you on tomorrow" 
