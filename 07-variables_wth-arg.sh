4ca7a30722b6ad83f180d9455dda23d8c6057d75
hari <hari@gmail.com>
2023-12-15 12:10:51 +0000

    chnges

---
+++ 07-variables_wth-arg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#We use the arguments to  give passwords during run time instead of saving it in the file.
+#How to enter commands during run time.
+
+echo "Please enter your username"
+
+read USERNAME # The name entered above will be automatically attached to USERNAME variable.
