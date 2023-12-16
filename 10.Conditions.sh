3f184788aacc5761348531b55b9e475c0aa1c706
hari <hari@gmail.com>
2023-12-16 07:10:48 +0000

    matter rasko

---
+++ 10.Conditions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#if condition syntax
+#if(expression){
+#    Statement if expression is true
+#}
+#else {
+#   Statement if expression is flase
+#}
+
+NUMBER=$1
+
+if ( $NUMBER -ge 50 ){
+    echo "The $NUMBER is greater than 50"
+}
+else {
+    echo "The $NUMBER is less than 50"
+}
+
+
