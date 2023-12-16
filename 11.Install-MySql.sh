fd489da2e9ed6993c2971f59fae55b53c58b7972
hari <hari@gmail.com>
2023-12-16 10:28:59 +0000

    changes are modified

--- 11.Install-MySql.sh
+++ 11.Install-MySql.sh
@@ -7,4 +7,16 @@ then
  echo "Error: Pleae run as root user"
 else 
  echo "Suessful: You are a root user"
-fi
+fi #Reverse of if indicates ending of condition.
+#Shell script won't stop if it faces errors, al other languages will stop. 
+#So we use exit status command to check previous command is success or not.
+#Shell script will store the command output in exit status and Command for exit status is $?
+
+yum install mysql -y
+
+if [ $? = 0 ]
+then
+   echo "The installation is sucessful"
+else
+   echo "The command is failed"
+
