326fb31b8d3abfceebf05338cb94b6c31d901a94
hari <hari@gmail.com>
2023-12-20 15:31:50 +0000

    chnges

--- 16.Delete_Old_logfiles.sh
+++ 16.Delete_Old_logfiles.sh
@@ -7,7 +7,7 @@ then
    echo "Source directory: $SOURCE_DIR doesn't exits"
 fi
 
-FILES_TO_DELETE:$(find /tmp/Logsfolder/ -type f -mtime +14 -name "*.log") #To find 14days older files 
+FILES_TO_DELETE:$(find $SOURCE_DIR -type f -mtime +14 -name "*.log") #To find 14days older files 
 
 while IFS= read -r line #To read the del file output line by line IFS=Interal field seperator
 do 
