660b0ffa8d9471a5f37bdc3759b5688ba6632acd
hari <hari@gmail.com>
2023-12-20 16:07:49 +0000

    chnges

--- 16.Delete_Old_logfiles.sh
+++ 16.Delete_Old_logfiles.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-SOURCE_DIR="/tmp/log"
+SOURCE_DIR="/tmp/logs"
 
 if [ ! -d $SOURCE_DIR ] # !=Not, -d=directory
 then 
    echo "Source directory: $SOURCE_DIR doesn't exits"
 fi
 
-FILES_TO_DELETE:$(find $SOURCE_DIR -type f -mtime +14 -name "*.log") #To find 14days older files 
+FILES_TO_DELETE:$(find /tmp/logs -type f -mtime +14 -name "*.log") #To find 14days older files 
 
 while IFS= read -r line #To read the del file output line by line IFS=Interal field seperator
 do 
