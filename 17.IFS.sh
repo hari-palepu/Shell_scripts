914b64d32e7888f2af9e2433a56a4b39814d0bdf
hari <hari@gmail.com>
2023-12-21 11:55:45 +0000

    IFS

--- 17.IFS.sh
+++ 17.IFS.sh
@@ -9,7 +9,7 @@ fi
 
 FILES_TO_DELETE=$(find /tmp/logs/ -type f -mtime +1 -name "*.log") #To find 14days older files 
 
-while IFS=":" read -r line name id data
+while IFS="-" read -r line name id data
 do 
  echo "Username;$name"
  echo "userid:$id"
