fc4bdbdde77789ab88fe8f6b86e6f85723f7f431
hari <hari@gmail.com>
2023-12-21 11:53:27 +0000

    IFS

---
+++ 17.IFS.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+file=/etc/passwd
+
+if [ ! -f $file ] # !=Not, -d=directory
+then 
+   echo "Source directory: $file doesn't exits"
+fi
+
+FILES_TO_DELETE=$(find /tmp/logs/ -type f -mtime +1 -name "*.log") #To find 14days older files 
+
+while IFS=":" read -r line name id data
+do 
+ echo "Username;$name"
+ echo "userid:$id"
+ echo "data type:$data"
+done < $file
