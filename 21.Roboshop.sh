af89855e0dca23d5ab77bcc63348fe0ca4c12d0a
hari <hari@gmail.com>
2023-12-22 15:43:01 +0000

    adding roboshop all insatnces shell script

---
+++ 21.Roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880af
+SG_ID=sg-068f4d593bbb78b34
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalouge" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do 
+  echo "instance is: $i"
+  if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+  then 
+      INSTANCE_TYPE="t3.small"
+  else
+      INSTANCE_TYPE="t2.micro"
+  fi
+
+  aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-068f4d593bbb78b34
+
+done
