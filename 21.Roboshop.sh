499616ad1957c6edf250a0c0ca047eec4080ca15
hari <hari@gmail.com>
2023-12-23 08:56:19 +0000

    tags

--- 21.Roboshop.sh
+++ 21.Roboshop.sh
@@ -14,6 +14,6 @@ do
       INSTANCE_TYPE="t2.micro"
   fi
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-068f4d593bbb78b34 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAdress' --output text)
+IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-068f4d593bbb78b34 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 echo "$i: $IP_ADDRESS"
 done
